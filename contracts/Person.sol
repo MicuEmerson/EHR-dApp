6fdb6917abfdb699aee293927a0396ff37df51e4
MicuEmerson <micuemerson@gmail.com>
2019-02-26 17:43:14 +0200

    Added Person(partial implemented) SmartContract & Web3

---
+++ contracts/Person.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.4.22 <0.6.0;
+contract Person {
+    
+    //identity
+    string private firstName;
+    string private lastName;
+    string private SID; 
+    
+    //birthday
+    string private year;
+    string private month;
+    string private day;
+    
+    //contract
+    string private email;
+    string private telephone;
+    string private personAddress;
+    string private zip;
+    string private city;
+    
+    //HL7 v2 format data
+    string[] private medicalData;
+    
+    constructor(string memory _firstName, string memory _lastName, string memory _SID) public{
+        firstName = _firstName;
+        lastName = _lastName;
+        SID = _SID;
+    }
+    
+    function getFirstName() public view returns (string memory _firstName){
+        _firstName = firstName;
+    }
+    function getLastName() public view returns (string memory _lastName){
+        _lastName = lastName;
+    }
+    function getSID() public view returns (string memory _SID){
+        _SID = SID;
+    }
+}
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,22 +3,29 @@
     <h2>Pacient Smart Contract</h2>
 
     <form class="my-form">
-
       <!-- first-name / last-name -->
       <div class="form-row">
         <div class="form-group col-md-5">
           <label>First Name</label>
-          <input type="text" class="form-control" :class="validators.firstName" v-model="firstName" placeholder="John"> 
-          <div class="invalid-feedback">
-            First Name can contain only letters.
-          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="validators.firstName"
+            v-model="firstName"
+            placeholder="John"
+          >
+          <div class="invalid-feedback">First Name can contain only letters.</div>
         </div>
         <div class="form-group col-md-5">
           <label>Last Name</label>
-          <input type="text" class="form-control" :class="validators.lastName" v-model="lastName" placeholder="Smith">
-          <div class="invalid-feedback">
-            Last Name can contain only letters.
-          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="validators.lastName"
+            v-model="lastName"
+            placeholder="Smith"
+          >
+          <div class="invalid-feedback">Last Name can contain only letters.</div>
         </div>
       </div>
 
@@ -26,52 +33,44 @@
       <div class="form-row">
         <div class="form-group col-md-10">
           <label>SID</label>
-          <input type="text" class="form-control" :class="validators.SID" v-model="SID" placeholder="security identifier">
-          <div class="invalid-feedback">
-            SID can contain only digits.
-          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="validators.SID"
+            v-model="SID"
+            placeholder="security identifier"
+          >
+          <div class="invalid-feedback">SID can contain only digits.</div>
         </div>
       </div>
 
       <!-- year / month / day -->
-       <div class="form-row">
+      <div class="form-row">
         <div class="form-group col-md-4">
           <label for="inputCNP">Year</label>
-          <select class="custom-select" :class="validators.selectedYear" v-model="selectedYear" required>
-            <option disabled value="">Select year</option>
-            <option v-for="i in 2019" v-if="i >= 1935" :key="i">
-              {{i}}
-            </option>
+          <select class="custom-select" :class="validators.selectedYear" v-model="selectedYear">
+            <option disabled value>Select year</option>
+            <option v-for="i in 2019" v-if="i >= 1935" :key="i">{{i}}</option>
           </select>
-          <div class="invalid-feedback">
-            Please select year.
-          </div>
+          <div class="invalid-feedback">Please select year.</div>
         </div>
 
         <div class="form-group col-md-3">
           <label for="inputCNP">Month</label>
-          <select class="custom-select" :class="validators.selectedMonth" v-model="selectedMonth" required>
-            <option disabled value="">Select month</option>
-            <option v-for="i in 12" :key="i">
-              {{i}}
-            </option>
+          <select class="custom-select" :class="validators.selectedMonth" v-model="selectedMonth">
+            <option disabled value>Select month</option>
+            <option v-for="i in 12" :key="i">{{i}}</option>
           </select>
-          <div class="invalid-feedback">
-            Please select month.
-          </div>
+          <div class="invalid-feedback">Please select month.</div>
         </div>
 
         <div class="form-group col-md-3">
           <label>Day</label>
-          <select class="custom-select" :class="validators.selectedDay" v-model="selectedDay" required>
-            <option disabled value="">Select day</option>
-            <option v-for="i in 31" :key="i">
-              {{i}}
-            </option>
+          <select class="custom-select" :class="validators.selectedDay" v-model="selectedDay">
+            <option disabled value>Select day</option>
+            <option v-for="i in 31" :key="i">{{i}}</option>
           </select>
-          <div class="invalid-feedback">
-            Please select day.
-          </div>
+          <div class="invalid-feedback">Please select day.</div>
         </div>
       </div>
 
@@ -79,17 +78,25 @@
       <div class="form-row">
         <div class="form-group col-md-5">
           <label>Email</label>
-          <input type="text" class="form-control" :class="validators.email" v-model="email" placeholder="johnsmith@gmail.com">
-          <div class="invalid-feedback">
-            Invalid email, please use this form abcd@xyz.
-          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="validators.email"
+            v-model="email"
+            placeholder="johnsmith@gmail.com"
+          >
+          <div class="invalid-feedback">Invalid email, please use this form abcd@xyz.</div>
         </div>
         <div class="form-group col-md-5">
           <label>Telephone</label>
-          <input type="text" class="form-control" :class="validators.telephone" v-model="telephone" placeholder="+40...">
-          <div class="invalid-feedback">
-            Telephone number can contain only digits and + sign.
-          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="validators.telephone"
+            v-model="telephone"
+            placeholder="+40..."
+          >
+          <div class="invalid-feedback">Telephone number can contain only digits and + sign.</div>
         </div>
       </div>
 
@@ -97,49 +104,55 @@
       <div class="form-row">
         <div class="form-group col-md-10">
           <label>Address</label>
-          <input type="text" class="form-control" :class="validators.address" v-model="address" placeholder="full address">
-          <div class="invalid-feedback">
-            Address field cannot be empty.
-          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="validators.address"
+            v-model="address"
+            placeholder="full address"
+          >
+          <div class="invalid-feedback">Address field cannot be empty.</div>
         </div>
         <div class="form-group col-md-5">
           <label>City</label>
-          <select class="custom-select" :class="validators.selectedCity" v-model="selectedCity" required>
-            <option disabled value="">Select city</option>
+          <select class="custom-select" :class="validators.selectedCity" v-model="selectedCity">
+            <option disabled value>Select city</option>
             <option>Cluj</option>
             <option>Satu Mare</option>
             <option>Bucuresti</option>
           </select>
-          <div class="invalid-feedback">
-            Please select city.
-          </div>
+          <div class="invalid-feedback">Please select city.</div>
         </div>
-        
+
         <div class="form-group col-md-5">
           <label>Zip</label>
-          <input type="text" class="form-control" :class="validators.zip" v-model="zip" placeholder="12345">
-          <div class="invalid-feedback">
-            Zip code can contain only digits.
-          </div>
+          <input
+            type="text"
+            class="form-control"
+            :class="validators.zip"
+            v-model="zip"
+            placeholder="12345"
+          >
+          <div class="invalid-feedback">Zip code can contain only digits.</div>
         </div>
       </div>
 
       <button type="button" class="btn btn-outline-primary" @click="handleForm()">Register</button>
-
     </form>
   </div>
 </template>
 
 <script>
 //aici bagam web3
-import FieldValidator from "../utils/FieldValidator.js"
+import FieldValidator from "../utils/FieldValidator.js";
+import PersonWeb3 from "../utils/PersonWeb3.js";
 
 export default {
-  name: 'Register',
-  data(){
+  name: "Register",
+  data() {
     return {
       firstName: "",
-      lastName : "",
+      lastName: "",
       SID: "",
       selectedYear: "",
       selectedMonth: "",
@@ -150,9 +163,9 @@ export default {
       zip: "",
       selectedCity: "",
 
-      validators:{
+      validators: {
         firstName: "",
-        lastName : "",
+        lastName: "",
         SID: "",
         selectedYear: "",
         selectedMonth: "",
@@ -162,113 +175,106 @@ export default {
         address: "",
         zip: "",
         selectedCity: ""
+      },
+
+    };
+  },
+  mounted: function() {
+    this.$nextTick(function() {
+      PersonWeb3.load();
+    });
+  },
+  methods: {
+    handleForm: function() {
+      PersonWeb3.createNewPerson(this.firstName, this.lastName, this.SID);
+      if (this.checkFieldValidity()) {
+        // this.$router.push('Home')
+      } else {
+        //  this.$router.push('Home')
       }
+    },
+
+    checkFieldValidity: function() {
+      let fieldValidator = new FieldValidator();
+      let ok = true;
+
+      if (!fieldValidator.checkOnlyLetters(this.firstName)) {
+        this.validators.firstName = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.firstName = "";
+      }
+      if (!fieldValidator.checkOnlyLetters(this.lastName)) {
+        this.validators.lastName = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.lastName = "";
+      }
+      if (!fieldValidator.checkNumber(this.SID)) {
+        this.validators.SID = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.SID = "";
+      }
+      if (fieldValidator.checkEmpty(this.selectedYear)) {
+        this.validators.selectedYear = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.selectedYear = "";
+      }
+      if (fieldValidator.checkEmpty(this.selectedMonth)) {
+        this.validators.selectedMonth = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.selectedMonth = "";
+      }
+      if (fieldValidator.checkEmpty(this.selectedDay)) {
+        this.validators.selectedDay = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.selectedDay = "";
+      }
+      if (fieldValidator.checkEmpty(this.selectedCity)) {
+        this.validators.selectedCity = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.selectedCity = "";
+      }
+      if (!fieldValidator.checkEmail(this.email)) {
+        this.validators.email = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.email = "";
+      }
+      if (!fieldValidator.checkNumber(this.telephone)) {
+        this.validators.telephone = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.telephone = "";
+      }
+      if (fieldValidator.checkEmpty(this.address)) {
+        this.validators.address = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.address = "";
+      }
+      if (fieldValidator.checkEmpty(this.selectedCity)) {
+        this.validators.selectedCity = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.selectedCity = "";
+      }
+      if (!fieldValidator.checkNumber(this.zip)) {
+        this.validators.zip = "is-invalid";
+        ok = false;
+      } else {
+        this.validators.zip = "";
+      }
+
+      return ok;
     }
-  },
-  methods:{
-     handleForm: function() {
-         if(this.checkFieldValidity()){
-            this.$router.push('Home') 
-         }
-         else{
-           this.$router.push('Home') 
-         }
-     },
-
-     checkFieldValidity: function(){
-        let fieldValidator = new FieldValidator();
-        let ok = true;
-
-        if(!fieldValidator.checkOnlyLetters(this.firstName)){
-          this.validators.firstName = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.firstName = "";
-        }
-        if(!fieldValidator.checkOnlyLetters(this.lastName)){
-          this.validators.lastName = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.lastName = "";
-        }
-        if(!fieldValidator.checkNumber(this.SID)){
-          this.validators.SID = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.SID = "";
-        }
-        if(fieldValidator.checkEmpty(this.selectedYear)){
-          this.validators.selectedYear = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.selectedYear = "";
-        }
-        if(fieldValidator.checkEmpty(this.selectedMonth)){
-          this.validators.selectedMonth = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.selectedMonth = "";
-        }
-        if(fieldValidator.checkEmpty(this.selectedDay)){
-          this.validators.selectedDay = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.selectedDay = "";
-        }
-        if(fieldValidator.checkEmpty(this.selectedCity)){
-          this.validators.selectedCity = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.selectedCity = "";
-        }
-        if(!fieldValidator.checkEmail(this.email)){
-          this.validators.email = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.email = "";
-        }
-        if(!fieldValidator.checkNumber(this.telephone)){
-          this.validators.telephone = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.telephone = "";
-        }
-        if(fieldValidator.checkEmpty(this.address)){
-          this.validators.address = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.address = "";
-        }
-        if(fieldValidator.checkEmpty(this.selectedCity)){
-          this.validators.selectedCity = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.selectedCity = "";
-        }
-        if(!fieldValidator.checkNumber(this.zip)){
-          this.validators.zip = "is-invalid";
-          ok = false;
-        }
-        else{
-          this.validators.zip = "";
-        }
-
-        return ok;
-     }
-  },
-
-}
+  }
+};
 </script>
 
 <style scoped>
